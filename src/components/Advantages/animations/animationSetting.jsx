eaa02ca68e839b82cbb84ab61fb7c8240c69e441
sexbutton <vladik00848@gmail.com>
2024-10-30 15:30:41 +0500

    1

--- src/components/Advantages/animations/animationSetting.jsx
+++ src/components/Advantages/animations/animationSetting.jsx
@@ -1,7 +1,15 @@
-// Импорт компонента Lottie и JSON-файла
+import { useEffect, useState } from "react";
 import Lottie from "lottie-react";
 
 const AnimationSetting = ({ animation }) => {
+  const [isClient, setIsClient] = useState(false);
+
+  useEffect(() => {
+    setIsClient(true); // Устанавливаем состояние, когда компонент загружен на клиенте
+  }, []);
+
+  if (!isClient) return null; // Возвращаем null до загрузки на клиенте
+
   return (
     <div>
       <Lottie animationData={animation} loop={true} />
